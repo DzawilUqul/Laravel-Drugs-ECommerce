010b1aa6e97b112f0d3c24e339b0465822bf561e
bydewanto <bydewanto@gmail.com>
2024-06-06 14:26:32 +0700

    UI

--- app/Http/Controllers/EtalaseController.php
+++ app/Http/Controllers/EtalaseController.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Obat;
+use App\Models\User;
+use Illuminate\support\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 
 class EtalaseController extends Controller
 {
     public function index() {
-        return view('/user_menu/etalase');
+        if(!auth()->check() || auth()->user()->roles !== 'User'){
+            abort(404);
+        };
+
+        $user = Auth()->user();
+
+        $obats = Obat::all();
+        $logsMap = [];
+
+        foreach ($obats as $obat) {
+            $logsMap[] = [
+                'nama' => $obat->nama_obat,
+                'stok' => $obat->stok,
+                'price' => $obat->harga,
+            ];
+        }
+
+        foreach ($logsMap as $log) {
+            $data[] = $log;
+        }
+
+        return view('/user_menu/etalase',[
+            'data' => $data,
+        ]);
     }
 }
